7abc2ea5efc732bc4ba4f1cfc0cdc5afaf36e892
Tomasz bla Fortuna <bla@thera.be>
2009-12-25 16:28:14 +0100

    Cleaning repos; testcase a bit corrected.

--- tests/do_tests.sh
+++ tests/do_tests.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+echo "This command can modify your state! We will move your"
+echo '~/.otpasswd file into ~/.otpasswd_copy and then move it back.'
+echo "But we can't help if you have configured global state."
+echo 'We will also remove two directories: ./lcov and ./gcov.'
+echo
+echo 'Starting in 10 seconds'
+sleep 10
+
+mv ~/.otpasswd ~/.otpasswd_copy
+rm -rf lcov gcov
+
 echo "Rebuilding the project. Will remove 'gcov'/'lcov' directories also in 3 seconds"
 sleep 3
 
@@ -17,8 +28,6 @@ make || (echo "Build failed"; exit 1)
 make test 
 
 
-rm -rf lcov gcov
-
 # GCOV version:
 
 # mkdir gcov; cd gcov
@@ -30,3 +39,8 @@ rm -rf lcov gcov
 mkdir lcov
 lcov --directory . --capture --output-file otpasswd.info --test-name OTPasswdCoverage
 genhtml --prefix . --output-directory lcov/ --title "OTPasswd coverage test" --show-details otpasswd.info
+
+
+
+# Restore state
+mv ~/.otpasswd_copy ~/.otpasswd
