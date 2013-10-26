cec0bffef365ab3d8e2d816c0eb6f71557ce09d3
Tomasz bla Fortuna <bla@thera.be>
2013-10-26 17:32:04 +0200

    Debian package internals template.
    Used for creating a source debian package which can later
    be build into binary packages. Not yet finished.

--- examples/otpasswd_oob.sh
+++ examples/otpasswd_oob.sh
@@ -8,14 +8,26 @@
 # Passcode with extended alphabet can contain 
 # ' " ~ etc. Keep it safe!
 
+# Warning - contact data is settable by user. Make sure
+# to SANITIZE it correctly! If it's a phone number you can 
+# check it with regular expression
+
 # Exemplary simple OOB utility. (Polish 'Plus' operator internet gateway)
 # Change it before using.
 # Contacts are phone numbers in format "48xxxyyyzzz"
 
 SENDMAIL=/usr/sbin/sendmail # Update to match your system. 
 
-TO="<$1@text.plusgsm.pl>"
-echo -en "To:$TO\nFrom: OTP <Ted@thera.be>\nSubject: OTP password\n\nPasscode = $2\n" | $SENDMAIL "$TO" 
+# Sanitize contact data
+echo "$1" | egrep '^[0-9]+$' > /dev/null 
+if [ $? != 0 ] then
+	echo "Contact data is not a valid phone number"
+	exit 1
+fi
+
+# Send email
+TO="<$1@text.plusgsm.CHANGEME.pl>"
+echo -en "To:$TO\nFrom: OTP <Ted@SETME.be>\nSubject: OTP password\n\nPasscode = $2\n" | $SENDMAIL "$TO" 
 
 # Tests.
 # whoami >> /tmp/OOB_TEST
